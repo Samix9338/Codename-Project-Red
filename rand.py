cd18c062eceab9d6ef6b1cf54bd120aaec200270
Samix9338 <99287964+Samix9338@users.noreply.github.com>
2022-03-23 14:15:53 -0400

    Create rand.py

---
+++ rand.py
@@ -0,0 +1,16 @@
+import random
+
+lst = random.randint(1, 5)
+print(lst)
+
+
+if lst == 1:
+   print ('Hi Sam')
+elif lst == 2:
+  print ('Hi Djay')
+elif lst == 3:
+  print ('Hi Aiden')
+else:
+  print ('Hi Stranger')
+
+ 
